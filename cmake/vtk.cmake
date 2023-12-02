c95fd8dc46416a86a4a306f52d44e962769be499
Curve <fynnbwdt@gmail.com>
2023-12-03 00:32:35 +0100

    fix: skip vtk environment on non windows platforms

--- cmake/vtk.cmake
+++ cmake/vtk.cmake
@@ -1,4 +1,10 @@
 macro(setup_vtk_env TARGET OUTPUT)
+
+  if (NOT WIN32)
+    message(STATUS "[ViennaLS] Skipping VTK-Environment setup for ${TARGET} (Only required on Windows)")
+    return()
+  endif()
+
   message(STATUS "[ViennaLS] Setting up VTK for ${TARGET}")
   set(CMAKE_RUNTIME_OUTPUT_DIRECTORY $<1:${PROJECT_BINARY_DIR}/${OUTPUT}>)
 
