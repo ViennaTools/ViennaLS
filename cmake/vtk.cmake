1b19b0ecc46d305522e564ae9546aec7004b2618
Curve <fynnbwdt@gmail.com>
2024-03-10 21:32:15 +0100

    fix: formatting

--- cmake/vtk.cmake
+++ cmake/vtk.cmake
@@ -1,8 +1,8 @@
 macro(setup_vtk_env TARGET OUTPUT)
-    message(STATUS "[ViennaLS] Setting up VTK-Environment for ${TARGET}")
+  message(STATUS "[ViennaLS] Setting up VTK-Environment for ${TARGET}")
 
-    add_custom_command(
-      TARGET ${TARGET}
-      POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy_directory $<TARGET_FILE_DIR:VTK> ${OUTPUT})
+  add_custom_command(
+    TARGET ${TARGET}
+    POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_directory $<TARGET_FILE_DIR:VTK> ${OUTPUT})
 endmacro()
