fa5fb7b20f53bbf02b33737d23894a1526fbd709
Curve <fynnbwdt@gmail.com>
2024-03-11 00:25:30 +0100

    chore(vtk): add note

--- cmake/vtk.cmake
+++ cmake/vtk.cmake
@@ -1,6 +1,10 @@
 macro(setup_vtk_env TARGET OUTPUT)
   message(STATUS "[ViennaLS] Setting up VTK-Environment for ${TARGET}")
 
+  # We expect all of the VTK binaries to be present in the same directory to which "vtksys" is
+  # built. This is currently the case, and has been the case for prior vtk versions - However we
+  # should keep an eye on this.
+
   add_custom_command(
     TARGET ${TARGET}
     POST_BUILD
