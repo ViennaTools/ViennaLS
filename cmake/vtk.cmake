7541b8abedba25b13d29a1f9616b7e6c5fddc3bd
Curve <fynnbwdt@gmail.com>
2024-03-10 21:30:14 +0100

    refactor: expose `setup_vtk_env`, use generator expressions, fix folder name casing

--- cmake/vtk.cmake
+++ cmake/vtk.cmake
@@ -1,14 +1,8 @@
 macro(setup_vtk_env TARGET OUTPUT)
-  if(NOT WIN32)
-    message(
-      STATUS "[ViennaLS] Skipping VTK-Environment setup for ${TARGET} (Only required on Windows)")
-  else()
-    message(STATUS "[ViennaLS] Setting up VTK for ${TARGET}")
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY $<1:${PROJECT_BINARY_DIR}/${OUTPUT}>)
+    message(STATUS "[ViennaLS] Setting up VTK-Environment for ${TARGET}")
 
     add_custom_command(
       TARGET ${TARGET}
       POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy_directory ${VTK_LIBS} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-  endif()
+      COMMAND ${CMAKE_COMMAND} -E copy_directory $<TARGET_FILE_DIR:VTK> ${OUTPUT})
 endmacro()
